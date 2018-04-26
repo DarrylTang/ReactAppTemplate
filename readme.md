1217ea37447838ac0534f35b6a93d964cdffe83a
Darryl <darryltanggy@gmail.com>
2018-04-26 16:12:42 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+React.js and Node.js with Express app template. 
+
+It has modules set-up for testing such as mocha and karma. 
+There is a style library Foundation installed as well. 
+
+Want to create the same thing? <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/content">Follow the course</a>
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      /*This allows the application to reference the files easier when using the
+      require method.
+      To use this, simply add an object that points to the path of the file and 
+      then simply use the object name for requires in the future*/
+      Main: 'app/components/Main.jsx',
+      applicationStyles: 'app/styles/app.scss',
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
